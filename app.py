a59f008a4f539dab4a017fb0f90acbd20383424f
injili <nyarekigospel@gmail.com>
2024-03-04 14:10:50 +0300

    the home page is created

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import flask from Flask, redirect, render_template
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    This is the main function
+    """
+    return (render_template('index.html'))
+
+if __name__ == '__main__':
+    app.run(port=800, debug=True)
