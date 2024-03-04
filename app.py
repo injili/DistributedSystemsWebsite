6f46fb9cf0a76a5ad5ea9ead68a978c039d737bc
injili <nyarekigospel@gmail.com>
2024-03-04 18:29:27 +0300

    this is built

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+"""
+App Inintializations
+"""
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+db - SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 @app.route('/', strict_slashes=False)
 def home():
